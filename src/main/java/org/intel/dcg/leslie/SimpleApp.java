52ebd33e81fe4639608a5b91d70f30a49782fac2
leslie-fang <Leslie-fang92>
2018-02-28 05:27:26 +0800

    test spark

--- src/main/java/org/intel/dcg/leslie/SimpleApp.java
+++ src/main/java/org/intel/dcg/leslie/SimpleApp.java
@@ -2,18 +2,30 @@ package org.intel.dcg.leslie;
 /* SimpleApp.java */
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
-
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.SparkConf;
+import java.util.*;
 public class SimpleApp {
   public static void main(String[] args) {
-    String logFile = "/home/automation/spark/spark-2.2.1-bin-hadoop2.7/README.md"; // Should be some file on your system
-    SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
-    Dataset<String> logData = spark.read().textFile(logFile).cache();
-
-    long numAs = logData.filter(s -> s.contains("a")).count();
-    long numBs = logData.filter(s -> s.contains("b")).count();
-
-    System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
-
-    spark.stop();
-  }
+    //String logFile = "/home/automation/spark/spark-2.2.1-bin-hadoop2.7/README.md"; // Should be some file on your system
+    //SparkSession spark = SparkSession.builder().appName("Simple Application").getOrCreate();
+    //Dataset<String> logData = spark.read().textFile(logFile).cache();
+    //long numAs = logData.filter(s -> s.contains("a")).count();
+    //long numBs = logData.filter(s -> s.contains("b")).count();
+   // System.out.println("Lines with a: " + numAs + ", lines with b: " + numBs);
+   //
+   // spark.stop();
+   String master = "local";
+   String appName = "SimpleApp";
+   SparkConf conf = new SparkConf().setAppName(appName).setMaster(master);
+   JavaSparkContext sc = new JavaSparkContext(conf);
+   
+   List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
+   JavaRDD<Integer> distData = sc.parallelize(data);
+   System.out.println(distData.map(a -> a+1).reduce((a, b) -> a + b));
+   JavaRDD<String> distFile = sc.textFile("/home/automation/spark/spark-2.2.1-bin-hadoop2.7/README.md");
+   System.out.println(distFile.count());
+   
+ }
 }
